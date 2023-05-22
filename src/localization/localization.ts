94e4b1b8bf4a7293fb11edce9d44d61273437d80
guntars.puzulis <guntars.puzulis@tilde.lv>
2023-05-22 13:48:19 +0300

    Add missing localization configuration for languages like Islandic

--- src/localization/localization.ts
+++ src/localization/localization.ts
@@ -1,57 +1,59 @@
 import { ILocalizedLanguage } from '../js/interfaces/ILocalizedLanguage'
 import bg from './languages/bg'
+import cs from './languages/cs'
 import da from './languages/da'
 import de from './languages/de'
 import el from './languages/el'
 import en from './languages/en'
+import es from './languages/es'
 import et from './languages/et'
+import fi from './languages/fi'
 import fr from './languages/fr'
 import gd from './languages/gd'
-import lv from './languages/lv'
-import it from './languages/it'
 import hr from './languages/hr'
+import hu from './languages/hu'
+import is from './languages/is'
+import it from './languages/it'
 import lt from './languages/lt'
+import lv from './languages/lv'
 import mt from './languages/mt'
 import nl from './languages/nl'
 import pl from './languages/pl'
 import pt from './languages/pt'
 import ro from './languages/ro'
-import sv from './languages/sv'
+import ru from './languages/ru'
 import sk from './languages/sk'
 import sl from './languages/sl'
-import es from './languages/es'
-import cs from './languages/cs'
-import hu from './languages/hu'
-import fi from './languages/fi'
-import ru from './languages/ru'
+import sv from './languages/sv'
 import uk from './languages/uk'
 
 const localization: { [Key: string]: ILocalizedLanguage} = {
-  de,
-  en,
-  lv,
   bg,
+  cs,
   da,
-  et,
-  fr,
+  de,
   el,
+  en,
+  es,
+  et,
+  fi,
+  fr,
   gd,
-  it,
   hr,
+  hu,
+  is,
+  it,
   lt,
+  lv,
   mt,
   nl,
   pl,
   pt,
   ro,
-  sv,
+  ru,
   sk,
   sl,
-  es,
-  cs,
-  hu,
-  fi,
-  ru,
+  sv,
   uk
 }
 export default localization
